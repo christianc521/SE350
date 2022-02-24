e45014b5226e66dd86ae5cbea70cc4e152d11769
Christian Cruz <seseko98@gmail.com>
2022-02-24 16:31:55 -0600

    Updated TravelManager

--- Assignment1/src/TravelManager.java
+++ Assignment1/src/TravelManager.java
@@ -3,16 +3,20 @@ import airport.Airport;
 import exception.BadParameterException;
 import exception.NullParameterException;
 import flight.CommercialFlight;
+import flight.PassangerFlight;
 
 public class TravelManager {
     public static void main(String[] args) {
         try {
             Airline Spirit = new Airline("Spirit");
+            Airline United = new Airline("United");
             Airport Chicago = new Airport("ORD");
             Airport LA = new Airport("LAX");
             CommercialFlight SpringBreak = new CommercialFlight(Spirit, Chicago, LA);
+            PassangerFlight SummerBreak = new PassangerFlight(United, Chicago, LA, 20);
 
             System.out.print(SpringBreak);
+            System.out.print(SummerBreak);
         } catch (NullParameterException e) {
             e.printStackTrace();
         } catch (BadParameterException e) {
